29528c67c8f3df8e025ecb436b6aa33a3a80b4bc
Chris Nunez <cenunez47@gmail.com>
2017-09-17 18:06:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  
 end
+
+
+# def name
+#   @name
+# end
+#
+# def name=(new_name)
+#   @name = new_name
+# end
+#
+# def breed
+#   @breed
+# end
+#
+# def breed=(breed_type)
+#   @breed = breed_type
+# end
